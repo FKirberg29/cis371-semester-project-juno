3395eef1a63491a75517e2e9c2e8e8fc5c614eb7
Devin M <75272179+mDevinn@users.noreply.github.com>
2023-03-15 18:40:17 -0400

    Starting code w/ navbar

---
+++ hello-world/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+<Navbar :icon="icon" />
+<hr>
+
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import icon from './assets/icon.png';
+
+
+
+
+
+
+export default {
+  components: {
+    Navbar
+  },
+
+
+  data() {
+    return {
+      icon: icon
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ hello-world/src/components/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class ="navbar">
+    <div class="navbar-logo">
+      <img :src="icon" alt="icon">
+    </div>
+
+    <div class="nav-menu">
+      <ul>
+        <li><b>Products</b></li>
+        <li><b>About Us</b></li>
+        <li><b>Contact</b></li>  
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  padding: 0 20px;
+  background-color: #fff;
+
+}
+.navbar-logo {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+.nav-menu {
+  width: 40%;
+  padding: 0 20px;
+  font-size: 20px;
+}
+.nav-menu ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  font-family: 'Kanit', sans-serif;
+}
+
+.nav-menu ul li:hover {
+  position: relative;
+  cursor: pointer;
+}
+
+.nav-menu ul li::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 0%;
+  height: 2px;
+  background-color: #000;
+  transition: width 0.3s ease-in-out;
+}
+
+.nav-menu ul li:hover::after {
+  width: 100%;
+}
+
+  @media screen and (max-width: 1024px) {
+    .navbar {
+      flex-direction: column;
+      height: auto;
+    }
+    .nav-menu {
+    width: 80%;
+    padding: 0 20px;
+    font-size: 20px;
+    }
+}
+
+</style>
